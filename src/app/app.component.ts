8c8bd4c1f7b62e64740e2173d89a0bbca311ebd4
quydang1412 <quydang1412@gmail.com>
2022-07-03 11:30:06 +0700

    checkJavaScriptOk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewChecked, OnInit } from '@angular/core';
+import logger from 'src/assets/js/demoExport.js';
+
+// import readyFunction from 'src/assets/js/custom.js';
+// const jQuery = require('../assets/js/custom.js');
+// function ready() {
+
+//   readyFunction;
+
+// };
 
 @Component({
   selector: 'app-root',
@@ -7,5 +16,26 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
+  constructor() { }
+  readyFunction: any;
+
+
+  // export class AppComponent implements AfterViewChecked{
+  //   constructor(private elementRef: ElementRef){
+
+  //   }
+
+  //   ngAfterViewChecked(){
+  //     var s = document.createElement("script");
+  //     s.type = "text/javascript";
+  //     s.src = "../assets/js/custom.js";
+  //     this.elementRef.nativeElement.appendChild(s);
+  //   }
   title = 'EShopAngular';
+  ngOnInit(): void {
+    // ready();
+    // readyFunction;
+
+    logger();
+  }
 }
