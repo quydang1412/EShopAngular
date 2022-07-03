92d4ace45f2623d6b5e3a8ed8fac3e4f5bd8bae3
quydang1412 <quydang1412@gmail.com>
2022-07-04 03:46:53 +0700

    oldHttp

--- src/app/core/common/message.constants.ts
+++ src/app/core/common/message.constants.ts
@@ -1,4 +1,4 @@
-export class MessageContstants {
+export class MessageConstants {
     public static SYSTEM_ERROR_MSG = "Có lỗi kết nối đến máy chủ";
     public static CONFIRM_DELETE_MSG = "Bạn có chắc muốn xóa bản ghi này?";
     public static LOGIN_AGAIN_MSG = "Bạn hết phiên đăng nhập. Mời đăng nhập lại."
